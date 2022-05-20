bf72639933b74d218f9e41725c2ffc1038330702
José Hudson de Oliveira Guimarães Junior <junior.guimaraes@poli.ufrj.br>
2022-05-20 13:33:29 -0300

    Initial commit

---
+++ src/Conta.java
@@ -0,0 +1,86 @@
+public abstract class Conta {
+	
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total; //o static aqui é bem importante
+							  // pois se não tivesse, o numero total da conta
+							  // ficaria sempre em 1
+							  // a partir do momento que vc tem static
+							  // nao utiliza mais o "this" e sim o atributo 
+							  // global.
+	
+	
+	
+	public Conta (int agencia, int numero) {
+		Conta.total ++;
+		//System.out.println("o total de contas e " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		//System.out.println("estou criando uma conta " + this.numero);
+	}
+
+	public abstract void deposita(double valor);
+	
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		}else {
+		return false;
+		}
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+
+	}
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("nao pode valor <= 0");
+			return;
+		}
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		if (agencia <= 0) {
+			System.out.println("nao pode valor menor ou igual a 0");
+			return; 
+			//em alguns casos pode ser interessante não ter o "setAgencia e 
+			//setNumero, pois pode ser definido como algo imutável, ao 
+			//contrario do saldo, que esta sempre mudando.
+		}
+		this.agencia = agencia;
+	}
+	public void setTitular (Cliente titular) {
+		this.titular = titular;
+	}
+	public Cliente getTitular() {
+		return titular;
+	}
+	public static int getTotal() {
+		return Conta.total;
+		//não existe this dentro de static
+	}
+}
